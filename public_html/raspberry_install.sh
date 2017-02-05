fb5eb58253e8b9144d49ccb406ad506dca14c8ce
David Cook <dtcook@inbox.com>
2017-02-05 21:12:55 +0000

    Raspberry Pi Displays: Minor alterations
    Coniston want to switch from 8am to 10pm to 9am to 5pm as this is when the cafe area is
    generally open.  A way is needed to make the on/off times of the screens site specific
    (or start using a PIR sensor before too much longer).
    
    Also alter where the mouse is moved to before hiding the pointer as (1, 1), in the top-left
    corner, now causes the Firefox menu bar to appear - we move it down a little.

--- public_html/raspberry_install.sh
+++ public_html/raspberry_install.sh
@@ -96,7 +96,7 @@ sleep 5
 xdotool key --clearmodifiers F11
 
 # Move the mouse pointer out of the way
-xdotool mousemove 1 1
+xdotool mousemove 1 20
 
 # Remove the mouse pointer
 unclutter &
@@ -187,10 +187,10 @@ cat > /home/pi/crontab.txt <<-EOF
 # Ranges and comma-separated lists are allowed
 
 # Force screen off at night
-00 22 * * * sudo tvservice -o
+00 17 * * * sudo tvservice -o
 
 # Force screen on and disable automatic DPMS in the morning
-00 08 * * * sudo bash -c "tvservice -p; chvt 9; chvt 7"
+00 09 * * * sudo bash -c "tvservice -p; chvt 9; chvt 7"
 
 # Run auto-update script hourly at 12 minutes past the hour
 12 * * * * /home/pi/bin/bec_autoupdate.sh 2>&1 | tee -a ~/cron_autoupdate.log
